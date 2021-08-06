36a4214acddb2184b6395cf88bb9e8369ff5d95a
JorgeGtz <jorge.gutierrezor@gmail.com>
2021-08-06 11:34:10 -0500

    add test for read dataset wrong path

--- tests/tests.py
+++ tests/tests.py
@@ -6535,6 +6535,11 @@ def test_plantcv_io_random_subset_greater_than_len():
     with pytest.raises(RuntimeError):
         _ = pcv.io.random_subset(dataset=full_list, num=n_samples, seed=None)
 
+def test_plantcv_io_read_dataset_non_existent_path():
+    with pytest.raises(RuntimeError):
+        _ = pcv.io.read_dataset(source_path='./non_existent_dir', pattern='')
+
+@pytest.mark.parametrize("test_pattern,expected", [['', 5], ['5', 1]])
 def test_plantcv_io_read_dataset():
     cache_dir = os.path.join(TEST_TMPDIR, "test_plantcv_io_read_dataset")
     os.mkdir(cache_dir)
@@ -6545,7 +6550,7 @@ def test_plantcv_io_read_dataset():
         img = rng.integers(low=0, high=255, size=img_size, dtype=np.uint8, endpoint=True)
         cv2.imwrite(os.path.join(cache_dir, f"tmp_img_{i}.png"), img)
     img_paths = pcv.io.read_dataset(source_path=cache_dir, pattern='')
-    assert len(img_paths)==n_images
+    assert len(img_paths ) == expected
 
 
 # ##############################
